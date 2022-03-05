2e93944292d3108b8b5057e5aeacdb9aa215e514
pnvnd <peter@datacrunch.ca>
2022-03-04 22:55:12 -0500

    Fixed timezone conversion

--- graphql.py
+++ graphql.py
@@ -1,6 +1,7 @@
 import requests
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 
 API_Key = "NRAK-XXXXXXXXXXXXXXXXXXXXXXXXXXX"
 
@@ -10,7 +11,7 @@ headers = {"API-Key": f"{API_Key}", "Content-Type": "application/json"}
 apm_query = """query GetSpans {
   actor {
     account(id: 1234567) {
-      nrql(query: "SELECT timestamp, duration, request.uri from Transaction WHERE appName = 'FlaskApp - Heroku (APM)' SINCE 60 DAYS AGO LIMIT MAX") {
+      nrql(query: "SELECT timestamp, duration, request.uri from Transaction WHERE appName = 'FlaskApp - Heroku (APM)' SINCE 24 HOURS AGO LIMIT MAX") {
         results
       }
     }
@@ -25,9 +26,10 @@ if r.status_code == 200:
 else:
   raise Exception(f"Query failed to run with a {r.status_code}.")
 
-# Convert UNIX epoch time to YYYY-MM-DD HH:mm:ss AM/PM
+# Convert UNIX epoch time to YYYY-MM-DD HH:mm:ss AM/PM and convert timezone as needed
 df.sort_values(by=["timestamp"], inplace=True)
-df["timestamp"] = pd.to_datetime(df["timestamp"], unit="ms").dt.strftime(date_format = "%I:%M:%S %p")
+df["timestamp"] = pd.to_datetime(df["timestamp"], unit="ms")
+df["timestamp"] = df["timestamp"].dt.tz_localize("Etc/GMT+2").dt.tz_convert("America/Toronto").dt.strftime(date_format = "%I:%M:%S %p")
 #df.head()
 
 # Plot the counts.
@@ -39,6 +41,8 @@ ax.scatter(pd.to_datetime(df["timestamp"]), 1000*df[(df["request.uri"]=="/ping")
 plt.title("New Relic GraphQL Example", fontsize=16)
 plt.xlabel("Timestamp", fontsize=12)
 fig.autofmt_xdate()
+xfmt = mdates.DateFormatter('%I:%M %p')
+ax.xaxis.set_major_formatter(xfmt)
 plt.ylabel("Seconds", fontsize=12)
 plt.tick_params(axis="both", which="major", labelsize=16)
 ax.set_ylim(bottom=0)
